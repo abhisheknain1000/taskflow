52c5aa66acb406cd1b5960de6c2775ad89801f81
raaz <yadavraj@1244@gmail.com>
2026-05-17 12:49:59 +0530

    frontend deployment

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,23 +1,35 @@
 import axios from "axios";
 
-export const API = axios.create({
-  baseURL:
-    process.env.NEXT_PUBLIC_API_URL ||
-    "http://localhost:5000/api/v1",
-});
+export const API =
+  axios.create({
 
-API.interceptors.request.use((config) => {
+    baseURL:
+      process.env
+        .NEXT_PUBLIC_API_URL,
 
-  if (typeof window !== "undefined") {
+    withCredentials: true,
+  });
 
-    const token =
-      localStorage.getItem("token");
+API.interceptors.request.use(
+  (config) => {
 
-    if (token) {
-      config.headers.Authorization =
-        `Bearer ${token}`;
+    if (
+      typeof window !==
+      "undefined"
+    ) {
+
+      const token =
+        localStorage.getItem(
+          "token"
+        );
+
+      if (token) {
+
+        config.headers.Authorization =
+          `Bearer ${token}`;
+      }
     }
-  }
 
-  return config;
-});
+    return config;
+  }
+);
